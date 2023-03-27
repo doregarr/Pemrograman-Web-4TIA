917c81c8b2a9fb542ebc6921928af81f36fe3357
doregarr <117086027+doregarr@users.noreply.github.com>
2023-03-27 19:41:39 +0700

    Priado Siregar 221510021
    Tugas Operator Matematika

---
+++ penjumlahan.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h5>Aplikasi Penjumlahan Matematika</h5>
+    <form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit" value="proses">tambah</button>
+    </form>
+</body>
+</html>
